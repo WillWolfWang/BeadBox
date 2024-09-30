7d5c0f09e64f2546c9beaf059f003408a49b0ac0
wangzhenhua <WillWolf_Wang@126.com>
2024-09-30 16:00:40 +0800

    添加 SoundPool 播放音乐功能

--- app/src/main/java/com/will/beatbox/BeatBox.kt
+++ app/src/main/java/com/will/beatbox/BeatBox.kt
@@ -1,19 +1,27 @@
 package com.will.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+
+private const val MAX_SOUNDS = 5
 // AssetManager 类可以访问 assets 下的资源
 class BeatBox(private val assets: AssetManager) {
     val sounds: List<Sound?>
-
+    //
+    private val soundPool: SoundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)// 指定某个时刻同时播放多少个音频，如果尝试播放第 6 个，SoundPool 会停止播放最早播放的那个
+        .build()
     // init 方法位于主构造器之后，从构造器之前执行， 创建了对象后， init 函数就执行了
     init {
         sounds = loadSound()
     }
 
+    // 将 assets 下的文件全都加到缓存集合中
     private fun loadSound(): List<Sound?> {
         // 这里传入相对路径
         val soundNames = assets.list(SOUNDS_FOLDER)
@@ -38,10 +46,24 @@ class BeatBox(private val assets: AssetManager) {
 //                拼装文件路径
                 val assetPath = "$SOUNDS_FOLDER/$fileName"
                 val sound = Sound(assetPath)
+                // 将音频加载到 SoundPool 中
+                load(sound)
                 sounds.add(sound)
             }
-            sounds.add(null)
             return sounds
+        }
+    }
+    // 加载音乐到 SoundPool 中
+    private fun load(sound: Sound) {
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        // load 函数可以把音频文件载入 SoundPool 待播，为了方便管理，重播卸载音频文件，load 函数返回一个 int 型的 id
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
+    }
+    // 播放音频
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it, 1f, 1f, 1, 0, 1f)
         }
     }
 }
--- app/src/main/java/com/will/beatbox/Sound.kt
+++ app/src/main/java/com/will/beatbox/Sound.kt
@@ -2,7 +2,7 @@ package com.will.beatbox
 
 // 管理声音资源文件名、用户可见文件名
 private const val WAV = ".wav"
-class Sound(val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
     // 为了有效显示声音文件名，先使用 split 和 last 函数分离出文件名
     // 再用 removeSuffix 删除后缀
     val name = assetPath.split("/").last().removeSuffix(WAV)
